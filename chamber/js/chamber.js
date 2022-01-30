ffcd38d09b72c50d99769dd67447e596999a01e5
courtneymickelsen <lam15020@byui.edu>
2022-01-30 00:30:11 -0700

    save this stuff

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,6 +1,7 @@
-const lastMod = document.querySelector('#lastMod');
-lastMod.textContent = `Last Updated: ${document.lastModified}`;
+document.querySelector('#lastMod').textContent = `Last Updated: ${document.lastModified}`;
 
 var today = new Date();
 document.querySelector('#currentYear').textContent = `${today.getFullYear()}`;
 
+const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
+document.querySelector('#today').textContent = ` Today is ${fullDate}`;
