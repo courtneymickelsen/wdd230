cf35e30777da0b232e772cbb92b132032cb42ee5
courtneymickelsen <lam15020@byui.edu>
2022-02-01 19:05:17 -0700

    been a minute

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,7 +1,3 @@
-document.querySelector('#lastMod').textContent = `Last Updated: ${document.lastModified}`;
-
-var today = new Date();
-document.querySelector('#currentYear').textContent = `${today.getFullYear()}`;
-
-const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
-document.querySelector('#today').textContent = ` Today is ${fullDate}`;
+function toggleMenu() {
+    menu = document.getElementsByClassName('#menu')[0].classList.toggle('#responsive');
+}
---
+++ chamber/js/time.js
@@ -0,0 +1,7 @@
+document.querySelector('#lastMod').textContent = `Last Updated: ${document.lastModified}`;
+
+var today = new Date();
+document.querySelector('#currentYear').textContent = `${today.getFullYear()}`;
+
+const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
+document.querySelector('#today').textContent = `${fullDate}`;
