e33565edf49cd652f740f45ed35ab07e2173ddce
courtneymickelsen <lam15020@byui.edu>
2022-02-12 22:36:39 -0700

    finallyyy finished it

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,12 +1,21 @@
-const input = document.querySelector("#input");
-const addButton = document.querySelector("#addButton");
-const list = document.querySelector("#list");
+const input = document.querySelector(".input");
+const addButton = document.querySelector(".addButton");
+const list = document.querySelector(".list");
 
-addButton.addEventListener('click', function(){
+addButton.addEventListener('click', function() {
     let chapter = document.createElement('li');
     let deleteButton = document.createElement('button');
-    chapter.textContent = `${input.textContent}`;
-    deleteButton.textContent = "X";
-    deleteButton.addEventListener('click', );
+    
+    deleteButton.textContent = "❌";
+    chapter.textContent = input.value;
+    
+    chapter.appendChild(deleteButton);
     list.appendChild(chapter);
+    
+    deleteButton.addEventListener('click', function(){
+        list.removeChild(chapter);
+    });
+    
+    input.focus();
+    input.value = "";
 });
