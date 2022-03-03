c482125f4f7ae9dbfce757fbc99990e36c296c76
courtneymickelsen <lam15020@byui.edu>
2022-03-02 20:35:17 -0700

    whole bunch of changes :)

--- chamber/js/time.js
+++ chamber/js/time.js
@@ -5,3 +5,18 @@ document.querySelector('#currentYear').textContent = `${today.getFullYear()}`;
 
 const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
 document.querySelector('#today').textContent = `${fullDate}`;
+
+var weekday = today.getDay();
+const banner = document.querySelector('.meet-banner')
+
+if (weekday == 1) {
+    showBanner();
+}
+else if (weekday == 2) {
+    showBanner();
+}
+
+function showBanner() {
+    banner.style.display = "block";
+}
+
